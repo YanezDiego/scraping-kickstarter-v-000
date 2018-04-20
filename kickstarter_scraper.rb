bb9da4d33774900a39975a7980ff443a072d7cfa
Diego Yanez <diegosyanez92@gmail.com>
2018-04-20 20:06:25 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,5 +11,3 @@ end
 #titie: project.css("h2.bbcard_name strong a").text
 #image_link: project.css("div.project-thumbnail a img").attribute("src").value
 #percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
-
-create_project_hash
