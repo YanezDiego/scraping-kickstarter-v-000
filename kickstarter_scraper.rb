bab549e3d5667e962c20d9298630f1aeefdedc1f
Diego Yanez <diegosyanez92@gmail.com>
2018-04-20 20:04:16 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -9,5 +9,6 @@ def create_project_hash
   #projects: kickstarter.css("li.project.grid_4").first
   #titie: project.css("h2.bbcard_name strong a").text
   #image_link: project.css("div.project-thumbnail a img").attribute("src").value
+  #percent_funded: project.css("ul.project-stats li.first.funded strong").text
 end
 create_project_hash
