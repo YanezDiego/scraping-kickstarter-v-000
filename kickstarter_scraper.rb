b34d27193c1a1d734d3434d33ccccb0372002fe1
Diego Yanez <diegosyanez92@gmail.com>
2018-04-20 20:07:33 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,6 +5,9 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
   projects = {}
+
+  kickstarter.css("li.project.grid_4").each do |project
+
 end
 
 #projects: kickstarter.css("li.project.grid_4").first
